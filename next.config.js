7408ead57d6a1ab7968b4a6c7723684d4f795801
Morne Karg <morne.karg@gmail.com>
2026-02-10 17:03:08 +0200

    Configure Next.js to export as static HTML for Netlify

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE || 'standalone',
+  output: 'export', // Export as static HTML for Netlify
   experimental: {
     outputFileTracingRoot: path.join(__dirname, '../'),
   },
